9912713523ac655898651402ccde11e6a3da235e
arthu <jordnhouyon@outlook.fr>
2023-01-12 09:25:38 +0100

    141 ok

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+$a=0;
+
+for($a;$a<10;$a++){
+    echo $a."<br>";
+}
+
+
+$b=0;
+$c=22;
+
+while($b<20){
+    echo $b * $c . "<br>";
+}
+
+
+$d=100;
+$e=59;
+
+
+
+while($d <= 10){
+    echo $d *= $e;
+    $d--;
+}
+
+
+$f = 1;
+
+while($f < 10){
+    echo "<br>" .  $f += 0.5;
+}
+
+for ($g = 0;$g < 15; $g++){
+    echo "On y arrive presque<br>";
+}
+
+
+for ($h = 20;$h > 0; $h--){
+    echo "c'est presque bon<br>";
+}
+
+
+
+for ($i = 1;$i < 100; $i += 15){
+    echo "on tient le bon bout<br>";
+}
+echo "<br>";
+
+
+for ($i = 200;$i > 0; $i -= 12){
+    echo "Enfin!!!!<br>";
+}
+echo "<br>";
